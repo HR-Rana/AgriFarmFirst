425bae65a6659c9f5de9e459c84c3d44f4f83fbe
Rana <76555387+HR-Rana@users.noreply.github.com>
2023-11-16 21:24:39 +0600

    Add files via upload

--- js/Custom.js
+++ js/Custom.js
@@ -1,43 +1,4 @@
 
 
- $('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true
-});
 
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
-});
-
-//  owl carousel 
-   $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
-    }
-})
+  new WOW().init();
