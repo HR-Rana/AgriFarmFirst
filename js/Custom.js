1c369dc485b27af593ff7e6798a9a3d7a46c34a2
Rana <76555387+HR-Rana@users.noreply.github.com>
2023-11-21 11:55:26 +0600

    Add files via upload

--- js/Custom.js
+++ js/Custom.js
@@ -1,4 +1,16 @@
 
 
 
-  new WOW().init();
+// active nav links
+
+const PathName = window.location.pathname;
+const NavLink = 
+document.querySelectorAll(".nav-list a").forEach(link =>{
+  if (link.href.includes(`${PathName}`)) {
+    console.log(link)
+    link.classList.add("active");
+  }else if(link.href == " " || link.href == "#"){
+    link.classList.remove("active");
+  }
+
+})
